17d1f82a052a098b86a7a33574ed46836b1052ad
reujab <reujab@gmail.com>
2018-07-16 05:32:37 -0400

    added download_image()

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,22 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+extern crate dirs;
+extern crate reqwest;
+extern crate url;
+
+use std::error::Error;
+use std::fs::File;
+use url::Url;
+
+fn download_image(url: &Url) -> Result<String, Box<Error>> {
+    let cache_dir = dirs::cache_dir().ok_or("no cache dir")?;
+    let segments = url.path_segments().ok_or("no path segments")?;
+    let mut file_name = segments.last().ok_or("no file name")?;
+    if file_name.is_empty() {
+        file_name = "wallpaper";
     }
+    let file_path = cache_dir.join(file_name);
+
+    let mut file = File::create(&file_path)?;
+    reqwest::get(url.as_str())?.copy_to(&mut file)?;
+
+    Ok(file_path.to_str().to_owned().unwrap().into())
 }
